a5874883fa6066d84cd94a4b11fdf4cd1f997db3
Jake Zeal <jake@jakezeal.com>
2017-02-27 13:32:43 -0800

    add UIDevice+Utilities

---
+++ UIDevice+Utilities.playground/Contents.swift
@@ -0,0 +1,34 @@
+// UIDevice+Utilities
+// Find more useful Swift extensions at https://github.com/jakezeal/ReusableSwiftExtensions
+
+import UIKit
+
+extension UIDevice {
+    var iPhone: Bool {
+        return UIDevice().userInterfaceIdiom == .phone
+    }
+    
+    enum ScreenType: String {
+        case iPhone4
+        case iPhone5
+        case iPhone6
+        case iPhone6Plus
+        case Unknown
+    }
+    
+    var screenType: ScreenType {
+        guard iPhone else { return .Unknown}
+        switch UIScreen.main.nativeBounds.height {
+        case 960:
+            return .iPhone4
+        case 1136:
+            return .iPhone5
+        case 1334:
+            return .iPhone6
+        case 2208:
+            return .iPhone6Plus
+        default:
+            return .Unknown
+        }
+    }
+}

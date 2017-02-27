b9b0a2dea0516dd9394a65bb3f748de581a4779a
Jake Zeal <jake@jakezeal.com>
2017-02-27 13:41:52 -0800

    add String+Utilities

---
+++ String+Utilities.playground/Contents.swift
@@ -0,0 +1,14 @@
+// String+Utilities
+// Find more useful Swift extensions at https://github.com/jakezeal/ReusableSwiftExtensions
+
+
+import UIKit
+
+extension String {
+    func heightWithConstrainedWidth(width: CGFloat, font: UIFont) -> CGFloat {
+        let constraintRect = CGSize(width: width, height: .greatestFiniteMagnitude)
+        let boundingBox = self.boundingRect(with: constraintRect, options: .usesLineFragmentOrigin, attributes: [NSFontAttributeName: font], context: nil)
+        
+        return boundingBox.height
+    }
+}
--- UIView+Utilities.playground/Contents.swift
+++ UIView+Utilities.playground/Contents.swift
@@ -14,3 +14,4 @@ extension UIView {
         setNeedsUpdateConstraints()
     }
 }
+

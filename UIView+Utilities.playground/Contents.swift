80d88bcf359a577e473883eb4f792bbfe75a3d2a
Jake Zeal <jake@jakezeal.com>
2017-02-27 13:18:19 -0800

    add UIView+Utilities extension

---
+++ UIView+Utilities.playground/Contents.swift
@@ -0,0 +1,16 @@
+// UIView+Utilities
+// Find more useful extensions at https://github.com/jakezeal/ReusableSwiftExtensions
+
+
+import UIKit
+
+extension UIView {
+    // Add multiple subviews
+    func addSubviews(_ subviews: [UIView]) {
+        for subview in subviews {
+            addSubview(subview)
+        }
+        
+        setNeedsUpdateConstraints()
+    }
+}

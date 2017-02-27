0d16b78fc74e8a5b68fe7547afaa4279226b0c00
Jake Zeal <jake@jakezeal.com>
2017-02-27 13:44:06 -0800

    merge String+RemoveLastCharacters to String+Utilities

--- String+Utilities.playground/Contents.swift
+++ String+Utilities.playground/Contents.swift
@@ -5,10 +5,24 @@
 import UIKit
 
 extension String {
+    // Height with Constrained Width
     func heightWithConstrainedWidth(width: CGFloat, font: UIFont) -> CGFloat {
         let constraintRect = CGSize(width: width, height: .greatestFiniteMagnitude)
         let boundingBox = self.boundingRect(with: constraintRect, options: .usesLineFragmentOrigin, attributes: [NSFontAttributeName: font], context: nil)
         
         return boundingBox.height
     }
+    
+    // Remove Last Characters
+    mutating func removeLastCharacters(numberOfCharacters: Int) {
+        let startIndex = str.index(str.startIndex, offsetBy: characters.count-numberOfCharacters)
+        let endIndex = str.index(str.startIndex, offsetBy: characters.count-1)
+        let range = startIndex...endIndex
+        removeSubrange(range)
+    }
+    
 }
+
+// Remove last characters of string
+var str = "Hello, playground"
+str.removeLastCharacters(numberOfCharacters: 4)

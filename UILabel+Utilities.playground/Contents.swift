c37694d5121bbc8a83c26f13f8fde219778897e5
Jake Zeal <jake@jakezeal.com>
2017-02-27 13:27:05 -0800

    add UILabel+Utilities extension

---
+++ UILabel+Utilities.playground/Contents.swift
@@ -0,0 +1,16 @@
+// UILabel+Utilities
+// Find more useful Swift extensions at https://github.com/jakezeal/ReusableSwiftExtensions
+
+import UIKit
+
+extension UILabel {
+    
+    func set(kerningValue: CGFloat) {
+        let attributes: [String : Any] = [NSKernAttributeName : kerningValue,
+                                          NSFontAttributeName : font,
+                                          NSForegroundColorAttributeName: textColor]
+        
+        attributedText = NSAttributedString(string: text ?? "",
+                                            attributes: attributes)
+    }
+}

ed04c22a8d9a3e610852be5f027cf8d4c8e8b6e1
Jake Zeal <jake@jakezeal.com>
2017-02-27 13:45:58 -0800

    add Date+Utilities extension

---
+++ Date+Utilities.playground/Contents.swift
@@ -0,0 +1,60 @@
+// Date+Utilities
+// Find more useful Swift extensions at https://github.com/jakezeal/ReusableSwiftExtensions
+
+import Foundation
+
+extension Date {
+    /// Returns the amount of years from another date
+    func years(from date: Date) -> Int {
+        return Calendar.current.dateComponents([.year], from: date, to: self).year ?? 0
+    }
+    /// Returns the amount of months from another date
+    func months(from date: Date) -> Int {
+        return Calendar.current.dateComponents([.month], from: date, to: self).month ?? 0
+    }
+    /// Returns the amount of weeks from another date
+    func weeks(from date: Date) -> Int {
+        return Calendar.current.dateComponents([.weekOfYear], from: date, to: self).weekOfYear ?? 0
+    }
+    /// Returns the amount of days from another date
+    func days(from date: Date) -> Int {
+        return Calendar.current.dateComponents([.day], from: date, to: self).day ?? 0
+    }
+    /// Returns the amount of hours from another date
+    func hours(from date: Date) -> Int {
+        return Calendar.current.dateComponents([.hour], from: date, to: self).hour ?? 0
+    }
+    /// Returns the amount of minutes from another date
+    func minutes(from date: Date) -> Int {
+        return Calendar.current.dateComponents([.minute], from: date, to: self).minute ?? 0
+    }
+    /// Returns the amount of seconds from another date
+    func seconds(from date: Date) -> Int {
+        return Calendar.current.dateComponents([.second], from: date, to: self).second ?? 0
+    }
+    /// Returns the a custom time interval description from another date
+    func offset(from date: Date) -> String {
+        if years(from: date)   > 0 { return "\(years(from: date))y"   }
+        if months(from: date)  > 0 { return "\(months(from: date))M"  }
+        if weeks(from: date)   > 0 { return "\(weeks(from: date))w"   }
+        if days(from: date)    > 0 { return "\(days(from: date))d"    }
+        if hours(from: date)   > 0 { return "\(hours(from: date))h"   }
+        if minutes(from: date) > 0 { return "\(minutes(from: date))m" }
+        if seconds(from: date) > 0 { return "\(seconds(from: date))s" }
+        return ""
+    }
+    
+    var shortDateStringFromNow: String {
+        let now = Date()
+        let components = Calendar.current.dateComponents([.hour, .minute, .second], from: self, to: now)
+        let dateTuple = (components.hour ?? 0, components.minute ?? 0, components.second ?? 0)
+        
+        switch dateTuple {
+        case let (hours, _, _) where hours >= 24: return "Yesterday"
+        case let (hours, _, _) where hours >= 1: return "\(hours)h ago"
+        case let (0, minutes, _) where minutes >= 1: return "\(minutes)m ago"
+        case let (0, 0, seconds): return "\(seconds)s ago"
+        default: fatalError("Error: date could not be converted to short date string.")
+        }
+    }
+}

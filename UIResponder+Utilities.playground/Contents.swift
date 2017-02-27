46e90e46e6a9f39e3fbb6ad1e462fde9189a398a
Jake Zeal <jake@jakezeal.com>
2017-02-27 13:10:26 -0800

    add UIResponder+Utilities

---
+++ UIResponder+Utilities.playground/Contents.swift
@@ -0,0 +1,31 @@
+// UIResponder+Utilities
+
+import UIKit
+
+extension UIView {
+    class func toString() -> String {
+        let name = NSStringFromClass(self)
+        let components = name.components(separatedBy: ".")
+        
+        guard let classString = components.last
+            else { fatalError("Error: couldn't convert class name to string.") }
+        
+        return classString
+    }
+    
+    class func nibNamed(nibName: String) -> UINib {
+        return UINib(nibName: nibName, bundle: Bundle.main)
+    }
+    
+    class func nib() -> UINib {
+        return nibNamed(nibName: self.toString())
+    }
+    
+    class func instanceFromNib() -> UIView {
+        
+        let view = nib().instantiate(withOwner: nil, options: nil)[0] as! UIView
+        view.translatesAutoresizingMaskIntoConstraints = false
+        
+        return view
+    }
+}
